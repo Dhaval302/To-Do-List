fc33254e01cf2cf5cddbeaac5e39a8fe5fc21265
Dhaval Gupta <dhavalgupta302@gmail.com>
2020-07-08 12:38:33 +0530

    Basic done

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,41 @@
 //selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
 
 //event listeners
+todoButton.addEventListener('click', addTodo);
 
 //functions
+function addTodo(event){
+    //prevent from submitting
+    event.preventDefault();
+    console.log('Hello');
+    //todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //completed button, check mark
+    const completedButton = document.createElement("button");
+    //completedButton.innerText = "<i class="fa fa-check"></i>"; doesn't work
+    completedButton.innerHTML = '<i class="fa fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //check trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fa fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    //append to list
+    todoList.appendChild(todoDiv);
+
+    //clear previous value
+    todoInput.value="";
+}
